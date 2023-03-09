1d044d69579c261d93c8b9c5f92c226db71668ee
Kous92 <kous92@gmail.com>
2023-03-09 18:17:54 +0100

    Initialisation framework data et réorganisation répertoire

--- App/MeteoWeather/ViewController.swift
+++ App/MeteoWeather/ViewController.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
+import MeteoWeatherData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let test1 = WeatherData()
+        print(test1.getData())
     }
-
-
 }
-
---
+++ Framework/MeteoWeatherData/MeteoWeatherData/WeatherData.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherData.swift
+//  MeteoWeatherData
+//
+//  Created by Koussaïla Ben Mamar on 09/03/2023.
+//
+
+import Foundation
+
+public class WeatherData {
+    public init() {
+        
+    }
+    
+    public func getData() -> [String] {
+        return ["Paris", "Londres", "Madrid", "Monaco", "Rome", "Alger", "Abu Dhabi", "New York", "Tokyo"]
+    }
+}
