a4e15911f1e704acbb8cb4c2db6b0b930225f19b
Kous92 <kous92@gmail.com>
2023-03-09 18:17:54 +0100

    Initialisation framework data et réorganisation répertoire

---
+++ Framework/MeteoWeatherData/MeteoWeatherData/MeteoWeatherData.h
@@ -0,0 +1,18 @@
+//
+//  MeteoWeatherData.h
+//  MeteoWeatherData
+//
+//  Created by Koussaïla Ben Mamar on 09/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MeteoWeatherData.
+FOUNDATION_EXPORT double MeteoWeatherDataVersionNumber;
+
+//! Project version string for MeteoWeatherData.
+FOUNDATION_EXPORT const unsigned char MeteoWeatherDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MeteoWeatherData/PublicHeader.h>
+
+
