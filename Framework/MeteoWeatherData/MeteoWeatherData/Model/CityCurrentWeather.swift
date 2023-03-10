85682831aa902842158d0b00c7e69b5bceb9ed7c
Kous92 <kous92@gmail.com>
2023-03-10 19:30:09 +0100

    Mise en place des différentes requêtes HTTP et adaptation des modèles pour le décodage

--- Framework/MeteoWeatherData/MeteoWeatherData/Model/CityCurrentWeather.swift
+++ Framework/MeteoWeatherData/MeteoWeatherData/Model/CityCurrentWeather.swift
@@ -8,36 +8,36 @@
 import Foundation
 
 public struct CityCurrentWeather: Codable {
-    let coord: Coord
-    let weather: [Weather]
-    let base: String
-    let main: Main
-    let visibility: Int
-    let wind: Wind
+    let coord: Coord?
+    let weather: [Weather]?
+    let base: String?
+    let main: Main?
+    let visibility: Int?
+    let wind: Wind?
     let rain: Rain?
     let snow: Snow?
-    let clouds: Clouds
-    let dt: Int
-    let sys: Sys
-    let timezone, id: Int
-    let name: String
-    let cod: Int
+    let clouds: Clouds?
+    let dt: Int?
+    let sys: Sys?
+    let timezone, id: Int?
+    let name: String?
+    let cod: Int?
 }
 
 // MARK: - Clouds
 public struct Clouds: Codable {
-    let all: Int
+    let all: Int?
 }
 
 // MARK: - Coord
 public struct Coord: Codable {
-    let lon, lat: Double
+    let lon, lat: Double?
 }
 
 // MARK: - Main
 public struct Main: Codable {
-    let temp, feelsLike, tempMin, tempMax: Double
-    let pressure, humidity: Int
+    let temp, feelsLike, tempMin, tempMax: Double?
+    let pressure, humidity: Int?
 
     enum CodingKeys: String, CodingKey {
         case temp
@@ -72,20 +72,20 @@ public struct Snow: Codable {
 
 // MARK: - Sys
 public struct Sys: Codable {
-    let type, id: Int
-    let country: String
-    let sunrise, sunset: Int
+    let type, id: Int?
+    let country: String?
+    let sunrise, sunset: Int?
 }
 
 // MARK: - Weather
 public struct Weather: Codable {
-    let id: Int
-    let main, description, icon: String
+    let id: Int?
+    let main, description, icon: String?
 }
 
 // MARK: - Wind
 public struct Wind: Codable {
-    let speed: Double
-    let deg: Int
-    let gust: Double
+    let speed: Double?
+    let deg: Int?
+    let gust: Double?
 }
