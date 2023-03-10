fd3b8b98eec58bbf8011511746a419a6bae31772
Kous92 <kous92@gmail.com>
2023-03-10 11:08:04 +0100

    Définition des erreurs liées à l'API

---
+++ Framework/MeteoWeatherData/MeteoWeatherData/MeteoWeatherDataError.swift
@@ -0,0 +1,22 @@
+//
+//  MeteoWeatherDataError.swift
+//  MeteoWeatherData
+//
+//  Created by Koussaïla Ben Mamar on 10/03/2023.
+//
+
+import Foundation
+
+enum MeteoWeatherDataError: String, Error {
+    case parametersMissing = "Erreur 400: Paramètres manquants dans la requête."
+    case invalidApiKey = "Erreur 401: La clé d'API fournie est inactive, invalide ou inexistante."
+    case notFound = "Erreur 404: Aucun contenu disponible."
+    case tooManyRequests = "Erreur 429: Trop de requêtes ont été effectuées dans un laps de temps (60 appels/minute). Veuillez réessayer ultérieurement."
+    case serverError = "Erreur 500: Erreur serveur."
+    case apiError = "Une erreur est survenue."
+    case invalidURL = "Erreur: URL invalide."
+    case networkError = "Une erreur est survenue, pas de connexion Internet."
+    case decodeError = "Une erreur est survenue au décodage des données téléchargées."
+    case downloadError = "Une erreur est survenue au téléchargement des données."
+    case unknown = "Erreur inconnue."
+}
